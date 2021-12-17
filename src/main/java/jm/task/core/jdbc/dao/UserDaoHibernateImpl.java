e325c240c4e17a4964771b92a6c642d8f5fc4070
folkwolf <ibragimoffer@yandex.ru>
2021-12-17 15:54:37 +0500

    Task 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,42 +2,103 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private SessionFactory sf;
 
+    public UserDaoHibernateImpl() {
+        this.sf = Util.getSF();
     }
 
 
     @Override
     public void createUsersTable() {
-        
+        String sqlRequest = "create table users (id int auto_increment not null primary key , name varchar(60) not null, lastName varchar(60) not null , age int not null);";
+        try (Connection connect = Util.connectDB(); Statement statement = connect.createStatement()) {
+            statement.executeUpdate(sqlRequest);
+        } catch (SQLException ignore) {
+
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sqlRequest = "drop table users;";
+        try (Connection connect = Util.connectDB(); Statement statement = connect.createStatement()) {
+            statement.executeUpdate(sqlRequest);
+        } catch (SQLException ignore) {
 
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        User user = new User(name, lastName, age);
+        try (Session session = Util.getSF().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSF().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        List<User> userList = null;
+        try (Session session = Util.getSF().openSession()) {
+            transaction = session.beginTransaction();
+            userList = session.createQuery("from User").getResultList();
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSF().openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,18 +1,40 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
+    private static String path = "jdbc:mysql://localhost:3306/testdb_task";
+    private static String login = "admin";
+    private static String pass = "admin";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection connectDB() throws SQLException {
-        String login = "admin";
-        String pass = "admin";
-        String params = "jdbc:mysql://localhost:3306/testdb_task";
-        return DriverManager.getConnection(params, login, pass);
+        return DriverManager.getConnection(path, login, pass);
     }
 
+    public static SessionFactory getSF () {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
 
+            properties.setProperty(Environment.URL, path);
+            properties.setProperty(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            properties.setProperty(Environment.USER, login);
+            properties.setProperty(Environment.PASS, pass);
+            properties.setProperty(Environment.SHOW_SQL, "false");
+            properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
+            sessionFactory = configuration.addProperties(properties).addAnnotatedClass(User.class).buildSessionFactory();
+        }
 
+        return sessionFactory;
+    }
 }
